3ae3a361084f6570d770aa7c1943beffa8d9b996
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-06-28 18:38:39 -0300

    created dog and poop nodes

--- scenes/levels/ScoopPoop/ScoopPoop.gd
+++ scenes/levels/ScoopPoop/ScoopPoop.gd
@@ -1,16 +1,3 @@
-extends Node2D
+extends "res://scenes/levels/Level/Level.gd"
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+var mensagem = "Cate a caca"
